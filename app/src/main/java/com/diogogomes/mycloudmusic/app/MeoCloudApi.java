4e28301e47029a6fd1efa3ac06b0f1f38702bad7
Diogo Gomes <dgomes@ua.pt>
2014-04-24 00:25:52 +0100

    Added

---
+++ app/src/main/java/com/diogogomes/mycloudmusic/app/MeoCloudApi.java
@@ -0,0 +1,40 @@
+package com.diogogomes.mycloudmusic.app;
+
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+
+/**
+ * Created by dgomes on 23/04/14.
+ */
+
+public class MeoCloudApi extends DefaultApi10a {
+
+    @Override
+    public String getAccessTokenEndpoint()
+    {
+        return "https://cloudpt.pt/oauth/access_token";
+    }
+
+    @Override
+    public String getAuthorizationUrl(Token requestToken)
+    {
+        return "https://cloudpt.pt/oauth/authorize?oauth_token="+requestToken.getToken();
+    }
+
+    @Override
+    public String getRequestTokenEndpoint()
+    {
+        return "https://cloudpt.pt/oauth/request_token";
+    }
+
+    @Override
+    public Verb getRequestTokenVerb() {
+        return Verb.GET;
+    }
+
+    @Override
+    public Verb getAccessTokenVerb() {
+        return Verb.GET;
+    }
+}
